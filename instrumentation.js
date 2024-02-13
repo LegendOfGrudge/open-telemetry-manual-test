a7369c41eff29917084939528d5dd7f06b02484a
LegendOfGrudge <nicholasfletcher3682@yahoo.ca>
2024-02-13 16:23:36 -0600

    first commit

---
+++ instrumentation.js
@@ -0,0 +1,24 @@
+/*instrumentation.js*/
+const { NodeSDK } = require('@opentelemetry/sdk-node');
+const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-node');
+const {
+  PeriodicExportingMetricReader,
+  ConsoleMetricExporter,
+} = require('@opentelemetry/sdk-metrics');
+const { Resource } = require('@opentelemetry/resources');
+const {
+  SemanticResourceAttributes,
+} = require('@opentelemetry/semantic-conventions');
+
+const sdk = new NodeSDK({
+  resource: new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: 'dice-server',
+    [SemanticResourceAttributes.SERVICE_VERSION]: '0.1.0',
+  }),
+  traceExporter: new ConsoleSpanExporter(),
+  metricReader: new PeriodicExportingMetricReader({
+    exporter: new ConsoleMetricExporter(),
+  }),
+});
+
+sdk.start();
